c01e26889aea959e9f5082ea4459fa17475a2dfb
vadudduk <dvk7520000@gmail.com>
2022-03-24 14:38:56 +0530

    Initial Commit

--- src/main/java/com/springBoot/JenkinsIntegrationOperations/CustomPropertiesConfiguration.java
+++ src/main/java/com/springBoot/JenkinsIntegrationOperations/CustomPropertiesConfiguration.java
@@ -2,7 +2,6 @@ package com.springBoot.JenkinsIntegrationOperations;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
